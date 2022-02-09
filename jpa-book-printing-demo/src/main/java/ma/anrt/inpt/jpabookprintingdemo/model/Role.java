de48f04dfbe7a1bb5de17301f974a3eda79f6923
ADDI Kamal <57157566+addi-kamal@users.noreply.github.com>
2022-02-09 18:28:56 +0100

    Delete jpa-book-printing-demo directory

--- jpa-book-printing-demo/src/main/java/ma/anrt/inpt/jpabookprintingdemo/model/Role.java
+++
@@ -1,8 +0,0 @@
-package ma.anrt.inpt.jpabookprintingdemo.model;
-
-public enum Role {
-	USER,
-	ADMIN,
-	SYSMANAGER
-
-}
--- jpa-book-printing-demo/src/main/java/ma/anrt/inpt/jpabookprintingdemo/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.anrt.inpt.jpabookprintingdemo.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ma.anrt.inpt.jpabookprintingdemo.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
